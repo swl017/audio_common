324e2f21b6991188a15185cde3b301628cbd6246
Seungwook Lee <seungwook1024@gmail.com>
2022-06-02 03:22:34 +0900

    Fix bug

--- audio_capture/src/audio_pub.py
+++ audio_capture/src/audio_pub.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 '''
-@file   pyaudio_pub8.py
+@file   audio_pub.py
 @brief  Publish audio messages in AudioDataStringArray.
 @date   2022-06-01
 @author Seungwook Lee, USRG@KAIST
@@ -9,13 +9,13 @@
 import rospy
 import pyaudio
 
-from audio_common_msgs.msg import AudioData
 from audio_common_msgs.msg import AudioDataStringArray
+from audio_common_msgs.msg import AudioInfo
 
-class RosPyaudio:
+class WavAudioPub:
     def __init__(self, pub_hz):
-        self.chunk          = 1  # Record in chunks of 1024 samples
-        self.sample_format  = pyaudio.paInt16  # 16 bits per sample
+        self.chunk          = 1024  # Record in chunks of 1024 samples
+        self.sample_format  = pyaudio.paInt16  # 16, 32 supported
         self.channels       = 8
         self.fs             = 44100  # Record at 44100 samples per second
         self.seconds        = 1.0 / pub_hz
@@ -26,40 +26,34 @@ class RosPyaudio:
                                 frames_per_buffer=self.chunk,
                                 input=True)
 
-        self.frame_pub = rospy.Publisher("audio/all_channels", AudioDataStringArray, queue_size=1)
-        
-    def setHz(self, hz):
-        self.seconds = 1.0 / hz
+        self.frame_pub = rospy.Publisher("audio/all", AudioDataStringArray, queue_size=1)
+        self.info_pub  = rospy.Publisher("audio/all/info", AudioInfo, queue_size=1)
 
+        print('Node initiated')
+        
     def getAudioFrames(self):
 
         frames = []  # Initialize array to store frames
 
         # Store data in chunks for x seconds
-        print_once = True
         for i in range(0, int(self.fs / self.chunk * self.seconds)):
             data = self.stream.read(self.chunk)
-            if print_once:
-                print(data)
-                print(data[0])
-                print(type(data))
-                print(type(data[0]))
-                print_once = False
             frames.append(data)
-        print(type(frames))
-        print(type(frames[0]))
 
         self.publish(frames)
 
-
     def publish(self, frames):
-        # msg = AudioData()
         msg = AudioDataStringArray()
         msg.header.stamp = rospy.Time.now()
         msg.data = frames
         self.frame_pub.publish(msg)
-        
 
+        info = AudioInfo()
+        info.channels = self.channels
+        info.sample_rate = self.fs
+        info.sample_format = str(self.sample_format)
+        self.info_pub.publish(info)
+        
     def terminateStream(self):
         # Stop and close the stream 
         self.stream.stop_stream()
@@ -68,13 +62,14 @@ class RosPyaudio:
         self.p.terminate()
         print('Node terminated')
 
+
 def main():
     rospy.init_node('audio_pub')
 
     pub_hz = 10
     rate   = rospy.Rate(pub_hz) # 10hz
 
-    node = RosPyaudio(pub_hz)
+    node = WavAudioPub(pub_hz)
     while not rospy.is_shutdown():
         node.getAudioFrames()
         rate.sleep()
--- audio_capture/src/audio_sub.py
+++ audio_capture/src/audio_sub.py
@@ -1,58 +1,89 @@
 #!/usr/bin/env python
 '''
-@file   pyaudio_sub8.py
+@file   audio_sub.py
 @brief  Subscribe to audio messages and save to wave files.
 @date   2022-06-01
 @author Seungwook Lee, USRG@KAIST
+@todo   Minimize size of published data per channel(It's almost as large as a video stream now(>2MB/s)!)
 '''
 import rospy
 import pyaudio
 import wave
 import numpy as np
+import os
 
-from audio_common_msgs.msg import AudioData
-from audio_common_msgs.msg import AudioDataStringArray
+from audio_common_msgs.msg import AudioDataStringArray # A custom message for this use case
+from audio_common_msgs.msg import AudioInfo
 
-class RosPyaudio():
+class WavAudioSub():
     def __init__(self):
-        self.sample_format  = pyaudio.paInt16  # 16 bits per sample
-        self.channels       = 8
-        self.fs             = 44100  # Record at 44100 samples per second
-        self.p = pyaudio.PyAudio()  # Create an interface to PortAudio
-        self.ext_frames  = []
-        self.frame_count = 0
-        self.file_name   = None
+        self.sample_format = pyaudio.paInt16
+        self.channels      = 8
+        self.fs            = 44100              # Record at 44100 samples per second
+        self.p             = pyaudio.PyAudio()  # Create an interface to PortAudio
+        self.oldest_msg_time = None
+        self.ext_frames    = []
+        self.frame_count   = 0
 
-        self.frame_sub = rospy.Subscriber("audio/all_channels", AudioDataStringArray, self.audioSubCallback)
+        #### Parameters ####
+        self.file_length_sec = 2.0              # Pick one of the two: time length vs frame count
+        self.frame_count_thres = 10             # 10 frames corresponding to 1 second
+        self.file_path     = os.getcwd() + '/'
+
+        self.frame_sub     = rospy.Subscriber("audio/all", AudioDataStringArray, self.audioSubCallback)
+        self.info_sub      = rospy.Subscriber("audio/all/info", AudioInfo, self.infoSubCallback)
+        self.channel_pub   = [rospy.Publisher("audio/ch"+str(i), AudioDataStringArray, queue_size=1) for i in range(self.channels)]
+        
+        print('Node initiated')
+
+    def infoSubCallback(self, msg):
+        # Sync to audio_pub node's setting
+        self.sample_format = int(msg.sample_format)
+        self.channels      = msg.channels
+        self.sample_rate   = msg.sample_rate
+        self.file_name     = 'wave_'+ str(self.channels) + '_' + str(self.sample_format) + '_' + str(self.fs) + '_'
+        pass
         
     def audioSubCallback(self, msg):
+        if self.oldest_msg_time is None:
+            self.oldest_msg_time = rospy.Time.now()
         self.frame_count += 1
         self.concatFrames(msg)
 
     def concatFrames(self, msg):
-        time = msg.header.stamp.secs * 10**9 + msg.header.stamp.nsecs
         frames = msg.data
         self.ext_frames += frames
-        if self.frame_count > 10:
-            self.saveFile(self.ext_frames, time)
-            for i in range(self.channels):
-                ch_data = self.splitChannels(self.ext_frames, time, i)
-                self.savePerChannel(ch_data, time, i)
+
+        # Save file and split channels
+        time_since_oldest = rospy.Time.now() - self.oldest_msg_time
+        if time_since_oldest.to_sec() > self.file_length_sec: # interval by time
+        # if self.frame_count > self.frame_count_thres: # interval by numbers of received messages
+            # time = msg.header.stamp # timestamp at the end of the sound
+            time = self.oldest_msg_time # timestamp at the start of the sound
+            self.save_time = str(time.secs * 10**9 + time.nsecs)
+            try:
+                self.saveFile(self.ext_frames)
+                for i in range(self.channels):
+                    ch_data = self.splitChannels(self.ext_frames, i)
+                    self.savePerChannel(ch_data, i)
+                    self.pubPerChannel(ch_data, time, i)
+            except:
+                pass
             self.ext_frames  = []
             self.frame_count = 0
+            self.oldest_msg_time = None
 
-    def saveFile(self, frames, time):
+    def saveFile(self, frames):
         # Save the recorded data as a WAV file
-        self.file_name = 'pyaudio_saved_2_' + str(time)
-        wf = wave.open(self.file_name + '.wav', 'wb')
+        wf = wave.open(self.file_name + self.save_time + '.wav', 'wb')
         wf.setnchannels(self.channels)
         wf.setsampwidth(self.p.get_sample_size(self.sample_format))
         wf.setframerate(self.fs)
         wf.writeframes(b''.join(frames))
         wf.close()
-        print('save: '+self.file_name + '.wav')
+        print('saved: '+self.file_name + self.save_time + '.wav')
 
-    def splitChannels(self, sdata, time, channel):
+    def splitChannels(self, sdata, channel):
         depth = self.p.get_sample_size(self.sample_format)
         # Extract channel data (24-bit data not supported)
         typ = { 1: np.uint8, 2: np.uint16, 4: np.uint32 }.get(depth)
@@ -65,22 +96,28 @@ class RosPyaudio():
 
         return data[channel::self.channels]
 
-    def savePerChannel(self, ch_data, time, channel):
+    def savePerChannel(self, ch_data, channel):
         # Save channel to a separate file
-        file_name = self.file_name + '_' + str(channel) + '.wav'
+        file_name = self.file_name + self.save_time + '_' + str(channel) + '.wav'
         outwav = wave.open(file_name, 'w')
         outwav.setnchannels(1)
         outwav.setsampwidth(self.p.get_sample_size(self.sample_format)) # bytes per sample
         outwav.setframerate(self.fs)
         outwav.writeframes(ch_data.tostring())
         outwav.close()
-        print('saved file: '+file_name)
+        print('saved: '+file_name)
+
+    def pubPerChannel(self, ch_data, time, channel):
+        msg = AudioDataStringArray()
+        msg.header.stamp = time
+        msg.data         = ch_data.tostring()
+        self.channel_pub[channel].publish(msg)
 
 
 def main():
     rospy.init_node('audio_sub')
 
-    node = RosPyaudio()
+    node = WavAudioSub()
     rospy.spin()
 
 if __name__=="__main__":
